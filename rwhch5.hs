c63d42049a0106a99e7dab8b7bfe6650eca739b3
Jonathan Campbell <jonalfcam@gmail.com>
2013-07-17 10:46:05 -0700

    Trying this again

--- rwhch5.hs
+++ rwhch5.hs
@@ -5,3 +5,20 @@ data JValue = JString String
 	    | JObject [(String, JValue)]
 	    | JArray [JValue]
 	      deriving (Eq, Ord, Show)
+
+getInt (JNumber n) = Just (truncate n)
+getInt _           = Nothing
+
+getDouble (JNumber n) = Just n
+getDouble _           = Nothing
+
+getBool (JBool b) = Just b
+getBool _         = Nothing
+
+getObject (JObject o) = Just o
+getObject _           = Nothing
+
+getArray (JArray a) = Just a
+getArray _          = Nothing
+
+isNull v            = v == JNull
